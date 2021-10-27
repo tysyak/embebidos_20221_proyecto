6af624fdf53a2a71b8d5b2312e86a7a1065055ce
Cristian Romero <mascrit@gmail.com>
2021-10-27 00:10:50 -0500

    correcciones

--- src/gas/Gas.py
+++ src/gas/Gas.py
@@ -11,14 +11,14 @@ class Gas:
         self.tele_bot = tele_bot
         self.__conn = database
 
-    def sensor_listener(self, tele_bot):
+    def sensor_listener(self):
         while True:
             if (self.gas_sensor == 0.0):
-                records = self.__con.ejecutar_consulta(
+                records = self.__conn.ejecutar_consulta(
                     "SELECT chat_id FROM alta_notificaciones where humo is true")
                 self.led_status.blink(on_time=0.1,off_time=0.1,n=700)
                 if records:
                     for chat_id in records:
-                        tele_bot.send_message(chat_id, emojize("""
+                        self.tele_bot.send_message(chat_id, emojize("""
                         Se detecta humo o gas cerca de tu puerta :fuego:
                         """, language='es'))
