7c8627d5852662636c48d37a141dc8268464adc3
Cristian Romero <mascrit@gmail.com>
2021-11-27 17:58:37 -0600

    Se añade modulo de bluetooth y se prueba composiscion de telegram y bluedot en procesamiento en paralelo con exito

---
+++ src/bluetooth/Bluetooth.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python3
+
+from signal import pause
+from threading import Thread
+
+class Bluetooth(Thread):
+    def __init__(self, bluetooth, servo):
+        Thread.__init__(self)
+        self.bd = bluetooth
+        self.servo = servo
+
+    def dpad(self, pos):
+        if pos.top:
+            self.servo.max()
+            print("Abriendo con BT")
+        elif pos.bottom:
+            self.servo.min()
+            print("Cerrando con BT")
+        elif pos.left:
+            pass # google-assistan
+        else:
+            pass
+
+    def bluetooth_main(self):
+        self.bd.when_pressed = self.dpad
+        pause()
+
+    def run(self):
+        self.bluetooth_main()
